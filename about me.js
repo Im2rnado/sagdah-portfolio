dda138cb6f22a7487825d3938b0958b7b55311d4
Ahmeddmkhattab <ahmed.khattab631@gmail.com>
2024-12-27 17:08:24 +0200

    yarab yeshtaghal

--- about me.js
+++ about me.js
@@ -2,7 +2,24 @@ window.addEventListener('load', () => {
     const textElement = document.getElementById('animatedtext');
     const headerElement = document.getElementById('animatedheader');
     const imageElement = document.getElementById('imageanimated')
+
+    console.log('Animating text element');
     textElement.classList.add('animate');
-    headerElement.classList.add('animate');
+
+    console.log('Animating image element');
     imageElement.classList.add('animate');
+
+    console.log('Animating Header element');
+    headerElement.classList.add('animate');
+
+
+    if (!textElement.classList.contains('animate')) {
+        textElement.classList.add('animate');
+    }
+    if (!imageElement.classList.contains('animate')) {
+        imageElement.classList.add('animate');
+    }
+    if (!headerElement.classList.contains('animate')){
+        headerElement.classList.add('animate');
+    }
 });
