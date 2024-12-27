0b238b1fca606465dd6dae4a5a8fa72299c7183d
Ahmeddmkhattab <ahmed.khattab631@gmail.com>
2024-12-27 16:46:49 +0200

    akher haga

---
+++ about me.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const textElement = document.getElementById('animatedtext');
+    const headerElement = document.getElementById('animatedheader');
+    const imageElement = document.getElementById('imageanimated')
+    textElement.classList.add('animate');
+    headerElement.classList.add('animate');
+    imageElement.classList.add('animate');
+});
